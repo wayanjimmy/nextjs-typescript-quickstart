21f5eee72b6504cb03e597aeaed2d9e7257f60b3
Wayan Jimmy <jimmyeatcrab@gmail.com>
2019-02-19 19:48:43 +0800

    :zap: purgecss

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
+const withPlugins = require('next-compose-plugins')
 const withTypescript = require('@zeit/next-typescript')
 const withCSS = require('@zeit/next-css')
+const withPurgeCSS = require('next-purgecss')
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || []
+  }
+}
 
 const nextConfig = {
   target: process.env.BUILD_TARGET || 'server',
@@ -11,4 +19,27 @@ const nextConfig = {
   }
 }
 
-module.exports = withCSS(withTypescript(nextConfig))
+module.exports = withPlugins(
+  [
+    [
+      withCSS,
+      {
+        importLoaders: 1,
+        localIdentName: '[local]_[hash:base64:5]'
+      }
+    ],
+    [
+      withPurgeCSS,
+      {
+        extractors: [
+          {
+            extractor: TailwindExtractor,
+            extensions: ['html', 'js', 'css', 'tsx']
+          }
+        ]
+      }
+    ],
+    withTypescript
+  ],
+  nextConfig
+)
---
+++ pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, {
+  Head,
+  Main,
+  NextScript,
+  NextDocumentContext
+} from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: NextDocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <html>
+        <Head />
+        <body className="font-sans m-0">
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
+
+export default MyDocument
